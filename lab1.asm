7e81e090d884a330bd2ff0d9d73787d6b0520aec
Sumaiya Tasnim <90747318+SumaiyaTasnim000@users.noreply.github.com>
2024-11-12 19:59:58 +0600

    Moving different values across registers

---
+++ lab1.asm
@@ -0,0 +1,10 @@
+       MOV AX,1100h;   move value in AX
+       MOV bx, 1102h;  move value in bx
+       MOV AX, 111h;   replace value of AX
+       MOV bx, 1004h;  replace value of bx     
+       MOV bx, 1102h;  replace value of bx              
+       ADD AX, bx; adding two values and saves value in ax
+       MOV bx, 0111h; putting ax value in bx
+                 
+                                                     
+                                                     
